f654b16036309243359c3d2c0da82a2a4464fb2f
Seonghun Kang <lutis107@gmail.com>
2018-06-05 11:56:17 +0900

    pom.xml 설정, mariaDB 테스트 코드

---
+++ src/test/java/site/corin2/dbtest/MariaDBTest.java
@@ -0,0 +1,25 @@
+package site.corin2.dbtest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.Test;
+
+public class MariaDBTest {
+    private static final String DRIVER = "org.mariadb.jdbc.Driver";
+    private static final String URL = "jdbc:mariadb://192.168.0.43:3306/corin2";
+    private static final String USER = "corin2";
+    private static final String PASSWORD = "1004";
+    
+    @Test
+    public void testConnection() throws Exception {
+        Class.forName(DRIVER);
+        
+        try(Connection con =  DriverManager.getConnection(URL, USER,  PASSWORD)) {
+            System.out.println("성공");
+            System.out.println(con);
+        }catch(Exception e) {
+            System.out.println("에러발생");
+            e.printStackTrace();
+        }
+    }
+}
