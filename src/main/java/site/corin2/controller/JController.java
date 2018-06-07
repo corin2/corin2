ca32bbe7747f582ebf803c2cd82c44343c4fcf87
jjstudy <pinkmoon720@gmail.com>
2018-06-07 11:32:24 +0900

    커밋

--- src/main/java/site/corin2/controller/JController.java
+++ src/main/java/site/corin2/controller/JController.java
@@ -1,8 +1,16 @@
 package site.corin2.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +18,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Handles requests for the application home page.
@@ -39,7 +49,6 @@ public class JController {
 	@RequestMapping(value = "announce", method = RequestMethod.GET)
 	public String index( Model model) {
 		
-	
 		return "announce";
 	}
 	
@@ -48,4 +57,52 @@ public class JController {
 		
 		return "write";
 	}
+	
+	 @RequestMapping(value = "imageUpload", method = RequestMethod.GET)
+	    public void communityImageUpload(HttpServletRequest request, HttpServletResponse response, @RequestParam MultipartFile upload) {
+	 
+	        OutputStream out = null;
+	        PrintWriter printWriter = null;
+	        response.setCharacterEncoding("utf-8");
+	        response.setContentType("text/html;charset=utf-8");
+	 
+	        try{
+	 
+	            String fileName = upload.getOriginalFilename();
+	            byte[] bytes = upload.getBytes();
+	            String uploadPath = "저장경로/" + fileName;//저장경로
+	 
+	            out = new FileOutputStream(new File(uploadPath));
+	            out.write(bytes);
+	            String callback = request.getParameter("CKEditorFuncNum");
+	 
+	            printWriter = response.getWriter();
+	            String fileUrl = "저장한 URL경로/" + fileName;//url경로
+	 
+	            printWriter.println("<script type='text/javascript'>window.parent.CKEDITOR.tools.callFunction("
+	                    + callback
+	                    + ",'"
+	                    + fileUrl
+	                    + "','이미지를 업로드 하였습니다.'"
+	                    + ")</script>");
+	            printWriter.flush();
+	 
+	        }catch(IOException e){
+	            e.printStackTrace();
+	        } finally {
+	            try {
+	                if (out != null) {
+	                    out.close();
+	                }
+	                if (printWriter != null) {
+	                    printWriter.close();
+	                }
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	 
+	        return;
+	    }
+
 }
--- src/main/webapp/WEB-INF/views/write.jsp
+++ src/main/webapp/WEB-INF/views/write.jsp
@@ -1,56 +1,54 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>write</title>
-   <script src="https://cdn.ckeditor.com/4.9.2/standard/ckeditor.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   <script src="https://cdn.ckeditor.com/4.9.2/standard/ckeditor.js"></script>
    
 </head>
 <body>
 
-	<div class="container">
-	<table>
-		<tr>
-	 		<td>제목</td>
-	 		<td><input type="text" ></td>
-	 	<tr>
-	 		
-	 	<tr>
-	 		<td>내용</td>
-	 		<td >
-		 		<textarea name="editor1" class="ckeditor" wrap="virtual"></textarea>
-		 		<script type="text/javascript">
-					CKEDITOR.replace( 'editor1' ,{
-					      width:'100%',
-				          height:'400px',
-				          filebrowserImageUploadUrl: '/imageUpload' 
-					});
-				</script>
-	 		</td>
-	 	</tr>		
-			
+	<div class="container" style="margin-top: 60px;">
+	<form class="form-horizontal" role="form" id="editorForm" method="post" action="/">
+		<table  class="table table-bordered">
+			<tr>
+		 		<td>제목</td>
+		 		<td><input type="text" ></td>
+		 	<tr>	
+		 	<tr>
+		 		<td>내용</td>
+		 		<td>
+			 		<textarea name="editor1" class="ckeditor"></textarea>
+			 	</td> 			 	
+		 	</tr>		
+			<tr>
+				<td></td>
+				<td>
+						<input type="button" value="수정">					
+						<input type="submit" value="작성">
+						<input type="button" value="취소" OnClick="javascript:history.back(-1)">
+				</td>
+			</tr>		
+		</table>
+	</form>	
+
+ 
+	</div>
 	
-		<tr>	
-			<td>
-				<center>
-					<input type="button" value="수정">
-					<input type="button" value="작성">
-					<input type="button" value="취소">
-				</center>
-			</td>
-		<tr>	
-	</table>
-	
-	
-</div>	
-	
-	
-	
+	<script type="text/javascript">			    
+							CKEDITOR.replace( 'editor1',{
+							    	width:'100%',
+						            height:'400px',
+						            filebrowserImageUploadUrl: '/imageUpload'
+							});
+					</script>
 	
 	
 	
