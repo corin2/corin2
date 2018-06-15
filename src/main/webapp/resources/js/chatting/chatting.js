9dc29861709ed904022c8ed7053a0f95eaac0e5e
Seonghun Kang <lutis107@gmail.com>
2018-06-15 12:10:22 +0900

    프로젝트 내 팀 채팅 완료

--- src/main/webapp/resources/js/chatting/chatting.js
+++ src/main/webapp/resources/js/chatting/chatting.js
@@ -1,11 +1,5 @@
 $(function() {
-	
-	// 채팅 페이지 시작 시, 함수 콜
-	getUsers(69); // 멤버 가져오기 함수 TODO: projectNum 변경할 것.
-
-	
-	
-	
+		
 	// Firebase 초기화
 	// [초기화 시작]
 	var config = {
@@ -30,13 +24,14 @@ $(function() {
 	console.log("현재 유저: " + currentUser);
 	$('#currentUser').html(currentUser);
 	
-	// 이메일주소 . -> *로 변경
-	function convertEmail(userId) {
-		return userId.replace(".", "*");
-	}
+	// 채팅 페이지 시작 시, 함수 콜
+	getUsers(69); // 멤버 가져오기 함수 TODO: projectNum 변경할 것.
+	$('#mainDialogs').empty();
 	
 	// 프로젝트 내 멤버 정보 가져오기
 	function getUsers(projectNum) {
+		messages = db.child('messages/' + projectNum);
+		
 		$.ajax({
 			url:"showMemberUserProfile",
 			datatype:"JSON",
@@ -45,6 +40,7 @@ $(function() {
 				console.log(data);
 				$.each(data.data, function(index, obj) {
 					var userUid = convertEmail(obj.userId);
+					
 					initialData(userUid, obj);
 					updateUserList(userUid, obj);
 					updateInfo(userUid, projectNum);
@@ -86,10 +82,6 @@ $(function() {
 
 			// 팀원 추가
 			db.child('users').on('child_added', function(snapshot) {
-				var teamUser = snapshot.val();
-				teamUser.key = snapshot.key;
-				if(prop == teamUser.key) {
-					$('#userList').append("<div id=" + teamUser.key + ">" + "<h3>" + teamUser.username + "</h3>" + "</div>");
 					
 					// 사용자 클릭 시
 					$('#' + teamUser.key).click(function() {
@@ -123,16 +115,14 @@ $(function() {
 			})
 		}
 	}*/
-
-
-	// 메시지 변수
-	messages = db.child('messages/' + 'test');
+	
+	
 
 	// 메시지 보내기
 	function sendMessage() {
 		var text = $('#messageText');
 
-		messages.update({
+		messages.push({
 			username: currentUser,
 			text: text.val(),
 			timestamp: Date.now()
@@ -153,27 +143,28 @@ $(function() {
 		}
 	})
 
-	// 메시지 뿌리기
+	// 메시지 출력
 	function showMessage(snapshot) {
 		var message = snapshot.val();
 		$('#mainDialogs').append("<p>" + message.username + ": " + message.text + " (" + convertTime(message.timestamp) +")" + "</p>");
 	}
-
-	// 메시지 출력 (안쓰고있음)
-	function showData(user, data) {
-		for(var prop in data) {
-			$('#mainDialogs').append(user + ": " + data);
-		}
-	}
-
-	// 변경된 사항이 있을 시 메시지 출력
-	//messages.on('child_added', showMessage);
-
-	$('#input').click(function() {
-		addData();
-		alert("데이터 들어감");
-	});
 	
+	// DB변동 시 메시지 출력
+	messages.on('child_added', showMessage);
+
+	
+	
+	//////////// [유틸 함수] ////////////
+	
+	// 이메일주소 . -> *로 변경
+	function convertEmail(userId) {
+		return userId.replace(".", "*");
+	}
+	
+    // 10미만 숫자 앞에 0 붙이기
+    function pad(n) {
+        return n > 9 ? "" + n: "0" + n;
+    }
 	
     // timestamp를 날짜 시간 으로 변환
     function convertTime(timestamp) {
@@ -197,25 +188,5 @@ $(function() {
 
         return convertDate + convertHour;
     }
-    
-    // 10미만 숫자 앞에 0 붙이기
-    function pad(n) {
-        return n > 9 ? "" + n: "0" + n;
-    }
-    
-    // 현재 시간을 yyyyMMddHHmmsss 형식으로 맞추기
-    function yyyyMMddHHmmsss() {
-        var vDate = new Date();
-        var yyyy = vDate.getFullYear().toString();
-        var MM = (vDate.getMonth() + 1).toString();
-        var dd = vDate.getDate().toString();
-        var HH = vDate.getHours().toString();
-        var mm = vDate.getMinutes().toString();
-
-        var ss = vDate.getSeconds().toString();
-        var sss= vDate.getMilliseconds().toString();
-        return yyyy + (MM[1] ? MM : '0'+MM[0]) + (dd[1] ? dd : '0'+dd[0]) + (HH[1] ? HH : '0'+ HH[0])
-            + (mm[1] ? mm : '0'+ mm[0]) + (ss[1] ? ss : '0'+ss[0])+ sss;
-    };
 
 }); // end - jQuery
