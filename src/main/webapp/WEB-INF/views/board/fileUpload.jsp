c834d891b132ad13a098618621ab7b1eddcc6a78
jjstudy <pinkmoon720@gmail.com>
2018-06-21 22:22:01 +0900

    파일함 디자인 커밋

--- src/main/webapp/WEB-INF/views/board/fileUpload.jsp
+++ src/main/webapp/WEB-INF/views/board/fileUpload.jsp
@@ -1,5 +1,7 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" 
+pageEncoding="UTF-8"%>
+
+ 
 <!-- fileUpload -->
 <script src="resources/js/board/vendor/jquery.ui.widget.js"></script>
 <script src="resources/js/board/jquery.iframe-transport.js"></script>
@@ -8,23 +10,32 @@
 <link href="resources/css/board/dropzone.css" type="text/css" rel="stylesheet" />
 
 
-	 <div class="container">
 	
-	 </div>
-
- 		<form action="upload" method="post" enctype="multipart/form-data"> 
-	    	<input id="fileupload" type="file" name="files[]"  multiple>
- 			<input type="hidden" name="projectNum" id="hiddenProjectNum" value="${sessionScope.sessionProjectNum}" >
- 			<input type="hidden" name="userId"id="hiddenUserId" value="${pageContext.request.userPrincipal.name}" >
- 	    </form>
-
-
-
-
-	<div id="dropzone" class="fade well" style="width: 200px;height: 200px;">Drop files here</div>
-	<div class="dropzonediv">	 
-
 	
+ 	 <form>
+ 	 	<input type="text" placeholder="검색" />
+ 	 	<input type="submit" value="검색"></input>
+	 </form>
+
+	<br>
+	<br>
+	<div class ="wrap" >
+		 	 <form action="upload" method="post" enctype="multipart/form-data"> 
+					<div class="filebox"> 
+						<label for="fileupload">파일함</label> 
+					    <input id="fileupload" type="file" name="files[]"  multiple>
+					</div>
+		 			<input type="hidden" name="projectNum" id="hiddenProjectNum" value="${sessionScope.sessionProjectNum}" >
+		 			<input type="hidden" name="userId"id="hiddenUserId" value="${pageContext.request.userPrincipal.name}" >
+		 	 </form>
+		<div id="dropzone" class="fade well" style='width:100%;height:100%;'>
+			 <div class ="over">
+				<img class="dropzoneimg" ></img>
+				
+				<div class="dropzonediv">	 
+				</div>
+			 </div>	
+		</div>
 	</div>
 	
 			
--- src/main/webapp/resources/js/board/myuploadfunction.js
+++ src/main/webapp/resources/js/board/myuploadfunction.js
@@ -4,12 +4,12 @@ $(function () {
 
 });
 
-//드래그로 파일 업로드 ( DB에 저장 )
+//드래그로 파일 업로드 ( DB에 저장 된는 동시에 파일뿌리기)
 function fileselect(){
 
 	$('#fileupload').fileupload({
      dataType: 'json',
-   
+ 
      done: function (e,data) {	  
      $(".dropzonediv").empty();
        
@@ -17,16 +17,20 @@ function fileselect(){
     	
     	 $.each(data.result, function(index , file) {
 			 html +=
-				  "<div class='dropzonechild' style='float:left;background-color:lightgray;width:300px;height:200px;margin-right:50px;margin-bottom:50px;border-radius:20px;'>"
-				  +"<div style='margin-left:220px;margin-top:5px'><img style='width:30px;height:30px;margin-right:3px'src='resources/images/board/download.png' onclick=download('" + file.uploadAlias + "')" 
+				"<div class='dropzonechild' style='border:1px solid #566270;word-wrap: break-word;float:left;background-color:lightgray;width:300px;height:200px;margin-left:60px;margin-right:20px;margin-bottom:100px;border-radius:20px;'>"
+				  +"<div style='word-wrap: break-word;margin-left:200px;margin-top:5px'>" 
+				  +"<img style='word-wrap: break-word;width:30px;height:30px;margin-right:3px'src='resources/images/board/download.png' onclick=download('" + file.uploadAlias + "')" 
 				  +"></img>"
-	    		  +"<img style='width:30px;height:30px;'src='resources/images/board/delete.png' onclick='deleteFile("+file.boardNum+")'></img></div>"
-	    		  +"<div class ='dropzonecontent' style='float:left;width:200px;height:100px;background-color:white;vertical-align:middle;text-align:center;color:black;border: 1px solid white;margin-left:50px;margin-top:20px;border-radius:10px;'>"
-	    		  +"<p style='text-align:center;margin-top:20px;color:black;font-size:30px;'>"+file.uploadOrigin+"</p>"
-	    		  +"<p><img class='img-circle' style='width:30px;hight:30px;margin-right:10px;' src='resources/images/"+file.userProfile+"'><label style='margin-top:30px;color:black'>"+file.userName+"</label></p></img>"
-	    		  +"<p style='color:black;'>"+file.boardDate+"</p>"
+				  +"<img class='thumbnail' style='display:inline;width:30px;height:30px;border: 1px solid lightgray; margin: 0; padding: 0;background-color:lightgray;borderword-wrap: break-word;'src='resources/images/board/see.png' onmouseover=preview('"+file.uploadAlias+"')></img>"
+	    		  +"<img style='word-wrap: break-word;width:30px;height:30px;'src='resources/images/board/delete.png' onclick='deleteFile("+file.boardNum+")'></img>"
+	    		  +"</div>"
+	    		  +"<div class ='dropzonecontent' style='word-wrap: break-word;float:left;width:200px;height:100px;background-color:white;vertical-align:middle;text-align:center;color:black;border: 1px solid white;margin-left:50px;border-radius:10px;'>"
+	    		  +"<p style='word-wrap: break-word;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;text-align:center;margin-top:20px;color:black;font-size:30px;'>"+file.uploadOrigin+"</p>"
+	    		  +"<p><img class='img-circle' style='word-wrap: break-word;width:30px;hight:30px;margin-right:10px;' src='resources/images/"+file.userProfile+"'><label style='margin-top:30px;color:black'>"+file.userName+"</label></p></img>"
+	    		  +"<p style='word-wrap: break-word;color:black;'>"+file.boardDate+"</p>"
 	    		  +"</div>"
 	    		  +"</div>"
+	
 		})
           $(".dropzonediv").html(html);  
         
@@ -38,8 +42,9 @@ function fileselect(){
 	
 }
 
+//파일 뿌리기
 function firstFileSelect() {
-  
+	  
     $.ajax({
     	type: "get",
     	url: "fileUpload1",
@@ -52,29 +57,63 @@ function firstFileSelect() {
     		 $(".dropzonediv").empty();
     		 $.each(data.file1, function(index , file) {
     			 html +=
-   				  "<div class='dropzonechild' style='float:left;background-color:lightgray;width:300px;height:200px;margin-right:50px;margin-bottom:50px;border-radius:20px;'>"
-   				  +"<div style='margin-left:220px;margin-top:5px'><img style='width:30px;height:30px;margin-right:3px'src='resources/images/board/download.png' onclick=download('" + file.uploadAlias + "')" 
-   				  +"></img>"
-   	    		  +"<img style='width:30px;height:30px;'src='resources/images/board/delete.png' onclick='deleteFile("+file.boardNum+")'></img></div>"
-   	    		  +"<div class ='dropzonecontent' style='float:left;width:200px;height:100px;background-color:white;vertical-align:middle;text-align:center;color:black;border: 1px solid white;margin-left:50px;margin-top:20px;border-radius:10px;'>"
-   	    		  +"<p style='text-align:center;margin-top:20px;color:black;font-size:30px;'>"+file.uploadOrigin+"</p>"
-   	    		  +"<p><img class='img-circle' style='width:30px;hight:30px;margin-right:10px;' src='resources/images/"+file.userProfile+"'><label style='margin-top:30px;color:black'>"+file.userName+"</label></p></img>"
-   	    		  +"<p style='color:black;'>"+file.boardDate+"</p>"
-   	    		  +"</div>"
-   	    		  +"</div>"
-   	    	
-   		})
+	    			"<div class='dropzonechild' style='border:1px solid #566270;word-wrap: break-word;float:left;background-color:lightgray;width:300px;height:200px;margin-left:60px;margin-right:20px;margin-bottom:100px;border-radius:20px;'>"
+	   				  +"<div style='word-wrap: break-word;margin-left:200px;margin-top:5px'>" 
+	   				  +"<img style='word-wrap: break-word;width:30px;height:30px;margin-right:3px'src='resources/images/board/download.png' onclick=download('" + file.uploadAlias + "')" 
+	   				  +"></img>"
+	   				  +"<img class='thumbnail' style='display:inline;width:30px;height:30px;border: 1px solid lightgray; margin: 0; padding: 0;background-color:lightgray;borderword-wrap: break-word;'src='resources/images/board/see.png' onmouseover=preview('"+file.uploadAlias+"')></img>"
+	   	    		  +"<img style='word-wrap: break-word;width:30px;height:30px;'src='resources/images/board/delete.png' onclick='deleteFile("+file.boardNum+")'></img>"
+	   	    		  +"</div>"
+	   	    		  +"<div class ='dropzonecontent' style='word-wrap: break-word;float:left;width:200px;height:100px;background-color:white;vertical-align:middle;text-align:center;color:black;border: 1px solid white;margin-left:50px;border-radius:10px;'>"
+	   	    		  +"<p style='word-wrap: break-word;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;text-align:center;margin-top:20px;color:black;font-size:30px;'>"+file.uploadOrigin+"</p>"
+	   	    		  +"<p><img class='img-circle' style='word-wrap: break-word;width:30px;hight:30px;margin-right:10px;' src='resources/images/"+file.userProfile+"'><label style='margin-top:30px;color:black'>"+file.userName+"</label></p></img>"
+	   	    		  +"<p style='word-wrap: break-word;color:black;'>"+file.boardDate+"</p>"
+	   	    		  +"</div>"
+	   	    		  +"</div>"
+   		
+    		 })
     		
 			$(".dropzonediv").html(html);  
     	}
  	   
     }); 
 }
+//이미지 미리보기 
+function preview(uploadAlias){
+	var xOffset = 10;
+    var yOffset = 30;
+
+    $(document).on("mouseover",".thumbnail",function(e){ //마우스 오버시
+     
+        $(".preview").remove();
+        $("body").append("<span class ='preview'><img src='resources/upload/"+ uploadAlias +"' width='350px' /></span>"); //보여줄 이미지를 선언                       
+        $(".preview")
+            .css("top",(e.pageY - xOffset) + "px")
+            .css("left",(e.pageX + yOffset) + "px")
+            .fadeIn("fast"); //미리보기 화면 설정 셋팅
+    });
+     
+    $(document).on("mousemove",".thumbnail",function(e){ //마우스 이동시
+        $(".preview")
+            .css("top",(e.pageY - xOffset) + "px")
+            .css("left",(e.pageX + yOffset) + "px");
+    });
+     
+    $(document).on("mouseout",".thumbnail",function(){ //마우스 아웃시
+        $(".preview").remove();
+    });
+      
+  
+}
+
 
 function download(uploadAlias) {
 
 	 location.href='download?fileName='+uploadAlias;
 }
+
+
+
 
 function deleteFile(boardNum) {
 	console.log(boardNum);
@@ -98,6 +137,6 @@ function deleteFile(boardNum) {
  	   
     }); 
     
-   
-    
+
 }
+ 
