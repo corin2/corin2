65c65629aa60e8c6b8ebee9662c27e62d38f2f8e
jinwon <jinwon220@naver.com>
2018-06-28 09:22:41 +0900

    방문자수 증가 생성 완료

--- src/main/java/site/corin2/statistics/service/StatisticsService.java
+++ src/main/java/site/corin2/statistics/service/StatisticsService.java
@@ -8,32 +8,26 @@
 package site.corin2.statistics.service;
 
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.context.ApplicationContext;
 
 import site.corin2.statistics.dao.StatisticsDAO;
 
-@Service
 public class StatisticsService {
-	
-	@Autowired
-	private SqlSession sqlsession;
-	
+
 	//방문자 카운터 수 증가
-	public void statisticsCntUpdate() {
-		System.out.println("44444444");
+	public void statisticsCntUpdate(ApplicationContext context) {
+		SqlSession sqlsession = (SqlSession)context.getBean("sqlSession");
+		System.out.println(sqlsession);
 		StatisticsDAO statisticsdao = sqlsession.getMapper(StatisticsDAO.class);
+		System.out.println(statisticsdao);
 		try {
 			int result = statisticsdao.firstVisit();
 			if(result == 0) {
-				System.out.println("55555555555");
 				statisticsdao.statisticsInsert();
 			}else if(result > 0) {
-				System.out.println("6666666");
 				statisticsdao.statisticsCntUpdate();
 			}
 		} catch (Exception e) {
-			System.out.println("77777777");
 			e.printStackTrace();
 		}
 	}
