153ac72cc912af85d16c6ec65a398925b1035855
jinwon <jinwon220@naver.com>
2018-07-04 15:13:48 +0900

    헤더js 헤더소켓js 주석

--- src/main/webapp/resources/js/header/headerSocket.js
+++ src/main/webapp/resources/js/header/headerSocket.js
@@ -4,6 +4,12 @@ $(function(){
 
 var wHeaderSocket;
 
+/**
+* @함수명 : headerconnect()
+* @작성일 : 2018. 06. 15.
+* @작성자 : 김 진 원
+* @설명 : 헤더에서 사용될 websocket 연결을 위한 함수
+**/
 function headerconnect() {
 	wHeaderSocket = new WebSocket("ws://localhost:8090/headerWebSocket?projectNum="+ sessionProjectNum);
 	wHeaderSocket.onopen = onHeaderOpen;
@@ -11,12 +17,25 @@ function headerconnect() {
 	wHeaderSocket.onclose = onHeaderClose;
 }
 
+/**
+* @함수명 : disHeaderconnect()
+* @작성일 : 2018. 06. 15.
+* @작성자 : 김 진 원
+* @설명 : 헤더소켓에서 빠져나갈때 실행
+**/
 function disHeaderconnect() {
 	wHeaderSocket.close();
 }
 
 function onHeaderOpen(evt) {}
 
+/**
+* @함수명 : onHeaderMessage(evt)
+* @작성일 : 2018. 06. 15.
+* @작성자 : 김 진 원
+* @설명 : 소켓에서 건네오는 데이터를 가공하여 실행 시킬 함수를 정의하는 곳.
+* @param evt - 소켓에서 건네오는 데이터가 담긴 json
+**/
 function onHeaderMessage(evt) {
 	var data = evt.data;
 	getChatUsers(); // 채팅 유저 리스트 불러오기
@@ -42,6 +61,13 @@ function onHeaderMessage(evt) {
 
 function onHeaderClose(evt) {}
 
+/**
+* @함수명 : sendHeader(msg)
+* @작성일 : 2018. 06. 15.
+* @작성자 : 김 진 원
+* @설명 : 소켓으로 보낼 데이터를 받아 보내주는 곳
+* @param msg - 소켓으로 보낼 데이터 값
+**/
 function sendHeader(msg) {
 	wHeaderSocket.send(msg);
 }
